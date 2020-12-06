614831dbf6ee5bfaa3f8abe50c5b4c39f0e5b86d
hilhorstt <tim.hilhorst@inspera.no>
2020-12-06 20:30:23 +0100

    Updated tech stack and captions

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -12,43 +12,48 @@ class TechStack extends React.PureComponent {
                         <h2 id="tech-stack">{i18n.t('techStack.title')}</h2>
                     </div>
                     <div className={styles.list}>
-                        <h3>Coding</h3>
+                        <h3>Languages, frameworks, libraries</h3>
                         <ul>
-                            <li className={styles.level1}>React, ES6</li>
-                            <li className={styles.level1}>Redux (saga)</li>
-                            <li className={styles.level1}>CSS3, SASS</li>
+                            <li className={styles.level1}>React, ES6, Redux (thunk & sagas)</li>
+                            <li className={styles.level1}>CSS3, SASS, CSS modules</li>
                             <li className={styles.level2}>NodeJS</li>
-                            <li className={styles.level3}>Backbone</li>
-                            <li className={styles.level3}>Bootstrap, underscoreJS</li>
-                            <li className={styles.level3}>Handlebars, Marionette, JQuery</li>
                             <li className={styles.level3}>Angular</li>
+                            <li className={styles.level3}>Backbone, Bootstrap, underscoreJS</li>
+                            <li className={styles.level3}>Handlebars, Marionette, JQuery</li>
+                            <li className={styles.level3}>Java, SQL, XSLT</li>
                         </ul>
                     </div>
                     <div className={styles.list}>
-                        <h3>Tooling</h3>
+                        <h3>Compilers, services & testing</h3>
                         <ul>
                             <li className={styles.level1}>Webpack, Gulp</li>
-                            <li className={styles.level2}>TeamCity</li>
-                            <li className={styles.level2}>EC2/S3 (AWS)</li>
-                            <li className={styles.level2}>Lambda (AWS)</li>
+                            <li className={styles.level1}>Jest, Mocha</li>
+                            <li className={styles.level2}>
+                                Puppeteer, BrowserStack, Percy, XCode, Postman
+                            </li>
+                            <li className={styles.level2}>
+                                EC2/S3, Lambda, Beanstalk, Route53 (AWS)
+                            </li>
+                            <li className={styles.level3}>TeamCity</li>
                         </ul>
                     </div>
                     <div className={styles.list}>
                         <h3>IDE & Design</h3>
                         <ul>
-                            <li className={styles.level1}>IntelliJ, WebStorm</li>
+                            <li className={styles.level1}>
+                                IntelliJ, WebStorm, Sublime, SQLDeveloper
+                            </li>
                             <li className={styles.level2}>
-                                Adobe Suite (Photoshop,
-                                Illustrator, InDesign,
-                                Premiere)
+                                Photoshop, Illustrator, InDesign, Premiere
                             </li>
                         </ul>
                     </div>
                     <div className={styles.list}>
-                        <h3>Process</h3>
+                        <h3>Agile, planning & process</h3>
                         <ul>
                             <li className={styles.level1}>Certified Scrum Master</li>
-                            <li className={styles.level2}>Product design</li>
+                            <li className={styles.level1}>JIRA, Confluence</li>
+                            <li className={styles.level2}>Product team</li>
                             <li className={styles.level2}>Backlog prioritisation</li>
                         </ul>
                     </div>
