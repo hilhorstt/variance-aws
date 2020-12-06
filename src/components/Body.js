c6aba1187c36c471b43685de52719e3d03f99baf
hilhorstt <tim.hilhorst@inspera.no>
2020-12-06 14:01:30 +0100

    Text updates, and dev server

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,17 +10,17 @@ class Body extends React.PureComponent {
                 <section className={`${styles.section} ${styles.welcome}`}>
                     <div className={styles.body}>
                         <div className={styles.logo}>
-                            <img src="images/variance-solutions-logo.svg" alt="Variance Solutions logo" />
+                            <img src={window.assetRoot + "images/variance-solutions-logo.svg"} alt="Variance Solutions logo" />
                         </div>
                     </div>
                     <div className={styles.title}>
-                        <h1>Lead front-end developer, accessibility auditor & certified scrum master</h1>
+                        <h1>Freelance lead front-end developer, accessibility auditor & certified scrum master</h1>
                     </div>
                 </section>
                 <section className={styles.section}>
                     <div className={styles.body}>
-                        <h2 id="personal-note">Personal note</h2>
-                        <p>My name is Tim Hilhorst, and welcome to my Variance Solutions company page.</p>
+                        <h2 id="personal-note">About Variance</h2>
+                        <p>My name is Tim Hilhorst, founder of Variance Solutions, and welcome to my company page.</p>
                         <p>I aim to combine design, accessibility and front-end technology from specification to realisation. As an architecture graduate of the Technical University of Delft, I have a solution-based approach to solving problems from sketch to detail.</p>
                         <p>I enjoy building and improving web applications that provide an inclusive experience for all its users. I do so by universal design, accessibility code standards, carrying out WCAG audits and writing readible, reusable code.</p>
                         <p>I enjoy discussing user experience, visual designs, meeting accessibility standards and the latest technologies in front-end development.</p>
@@ -38,6 +38,7 @@ class Body extends React.PureComponent {
                 <section className={`${styles.section} ${styles.welcome}`}>
                     <div className={styles.body}>
                         <h2 id="contact">Contact</h2>
+                        <p>I'm currently </p>
                         <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/company/variancesolutions">Variance Solutions</a><br/>
                         <strong>KvK registration:</strong> <a href="https://www.kvk.nl/orderstraat/product-kiezen/?kvknummer=757668090000&origq=variance">75766809</a><br/>
                         <strong>E-mail:</strong> <a href="mailto:tim@variancesolutions.nl">tim@variancesolutions.nl</a>
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,21 @@ const path = require('path');
 const ROOT_PATH = path.resolve(__dirname);
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
     return {
         mode: env,
         entry: './src/index.js',
         output: {
-            path: path.resolve(ROOT_PATH, 'static'),
+            path: path.resolve(ROOT_PATH, 'dist'),
             publicPath: 'http://localhost:3000/',
             filename: 'bundle.js',
         },
         devServer: {
-            contentBase: path.join(__dirname, 'static'),
+            contentBase: path.join(__dirname, 'dist'),
             compress: true,
+            hot: true,
             port: 3010
         },
         devtool: 'inline-source-map',
@@ -59,12 +61,17 @@ module.exports = env => {
             new MiniCssExtractPlugin({
                 filename: 'styles.css',
             }),
+            new HtmlWebpackPlugin({
+                title: 'Test Player',
+                template: path.resolve(ROOT_PATH, 'src/index.html'), // Load a custom template
+                inject: 'body', // Inject all scripts into the body
+            }),
             new CopyPlugin({
                 patterns: [
                     { from: 'src/images', to: '../dist/static/images', force: true },
                     { from: 'src/server.js', to: '../dist/server.js', toType: 'file' },
                     { from: 'package.json', to: '../dist/package.json', toType: 'file' },
-                    { from: 'index.html', to: '../dist/index.html', toType: 'file' },
+                    { from: 'src/index.html', to: '../dist/index.html', toType: 'file' },
                 ],
             }),
         ],
