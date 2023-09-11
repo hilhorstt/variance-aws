cec6ee23fda6aa5c2523c9a4fc7f0f20834658ce
hilhorstt <tim@variancesolutions.nl>
2023-09-11 16:44:28 +0200

    Global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Global, css } from '@emotion/react';
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { I18nProvider } from '../services/i18n';
@@ -9,22 +8,6 @@ function App({ locale, helmetContext }) {
     return (
         <HelmetProvider context={helmetContext}>
             <I18nProvider locale={locale}>
-                <Global
-                    styles={css`
-                      body {
-                        font-family: 'Arial', sans-serif;
-                        margin: 0;
-                        padding: 0;
-                        color: #353535;
-                      }
-                      h1, h2, h3, h4, h5, h6 {
-                        font-family: 'Roboto Slab', serif;
-                      }
-                      a {
-                          color: rgb(21, 98, 81);
-                      }
-                    `}
-                />
                 <Header />
                 <Body />
             </I18nProvider>
